edefee663096a5676ddc73c738b0fd92ee2bf920
lightning <admin@chenxyz.cn>
2024-01-30 20:21:49 +0800

    v1.1

--- themes/plog/config.js
+++ themes/plog/config.js
@@ -1,14 +1,13 @@
 const CONFIG = {
 
   // 菜单配置
-  MENU_CATEGORY: false, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: false, // 显示归档
-  MENU_SEARCH: true, // 显示搜索
-  MENU_RSS: false, // 显示订阅
-// v1.4
-  NAV_NOTION_ICON: true, // 是否读取Notion图标作为站点头像
-  USER_AVATER:'https://file.notion.so/f/f/2219ebb3-4960-4e74-8eac-c38638a4b144/45080d03-145c-4f68-badf-bdd5f69f1c5f/notion-avatar-1697279179931.svg?id=b96478f0-3c93-41a1-bddc-0a8fba4b5a62&table=block&spaceId=2219ebb3-4960-4e74-8eac-c38638a4b144&expirationTimestamp=1704729600000&signature=tzQPyUVkj4y_vn9V3WA7Z5h8la7zN0KBvaZxcJ00IBI&downloadName=notion-avatar-1697279179931.svg',  //文章页面头像
+  NOBELIUM_MENU_CATEGORY: false, // 显示分类
+  NOBELIUM_MENU_TAG: true, // 显示标签
+  NOBELIUM_MENU_ARCHIVE: false, // 显示归档
+  NOBELIUM_MENU_SEARCH: true, // 显示搜索
+  NOBELIUM_MENU_RSS: false, // 显示订阅
+
+  NOBELIUM_NAV_NOTION_ICON: true // 是否读取Notion图标作为站点头像
 
 }
 export default CONFIG
